f76701eaead0681821edd9c54d6004b17989eb01
Sviatoslav Kyrychuk <swyatoslav24042010@gmail.com>
2026-02-22 06:41:55 +0000

    track client as normal directory

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import Header from '@/components/Header'
+import { Suspense } from 'react'
+
+
+const RootLayout = () => (
+    <>
+        {/* <Header /> */}
+        <div className='m-10'>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Outlet />
+            </Suspense>
+        </div>
+        <TanStackRouterDevtools position="bottom-left" />
+        <ReactQueryDevtools initialIsOpen={false} />
+    </>
+)
+
+export const Route = createRootRoute({ component: RootLayout })
